6e021c48f6b718920851eb0b2a0a7e15dd8c08fc
anadulimited@gmail.com <anadulimited@gmail.com>
2025-09-10 16:07:58 +0100

    Created ceate and delete controller

--- server.js
+++ server.js
@@ -1,18 +1,23 @@
-require('dotenv').config()
-const express = require('express')
-const PORT = 1234
-const app = express()
-const mongoose = require('mongoose')
-const DB = process.env.DB_URI
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const userRouter = require("./router/userRouter");
 
+const PORT = 1234;
+const app = express();
+app.use(express.json());
+app.use("/user", userRouter);
 
+const DB = process.env.DB_URI;
 
-mongoose.connect(DB).then(()=>{
+mongoose
+  .connect(DB)
+  .then(() => {
     console.log("Database connected successfully");
-    app.listen(PORT,()=>{
-    console.log(`Server is running on PORT: ${PORT}`); 
-})
-}).catch((error)=>{
-    console.log("error connecting to database ",error.message);
-    
-})
+    app.listen(PORT, () => {
+      console.log(`Server is running on PORT: ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log("error connecting to database ", error.message);
+  });
